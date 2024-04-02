e9c29e883fbe240844af57c7abdce90c697bbda5
Nicolas Guilbaud <nicolas.guilbaud@ulb.be>
2024-04-02 12:41:20 +0200

    Adding clues & medicine tasks (untested)

--- project_pandemic_empty/pandemic.cydsn/main.c
+++ project_pandemic_empty/pandemic.cydsn/main.c
@@ -22,11 +22,37 @@
 
 /* Task definitions */
 #define GAME_TASK_NAME ("game_task")
+#define CLUES_TASK_NAME ("clues_task")
+#define MEDICINE_TASK_NAME ("medicine_task")
 
 #define GAME_PRIORITY (20)
+#define CLUES_PRIORITY (10)
+#define MEDICINE_PRIORITY (9)
+
+/* Number of pills required in stock before allocating lab to vaccine */
+#define PILLS_REQUIRED (0)
+
+/* Mutex for lab, which is a shared resource*/
+SemaphoreHandle_t labMutex;
+/* Semaphore to synchronize the clue processing task with the release of the clue
+ * Avoids the situation where a new clue is released before the previous one is processed
+ * by the lab, invaliding the medicine research.
+ */
+SemaphoreHandle_t clueSync;
+
+/* Represents the current active clue
+ * It is a shared ressource without the need of a mutex (no critical section)
+ */
+Token currentClue = 0;
+
+/* Task functions */
+void cluesTask( void * args);
+void medicineTask( void * args);
 
 /* Task handlers */
 TaskHandle_t gameHandler;
+TaskHandle_t cluesHandler;
+TaskHandle_t medicineHandler;
 
 /*
  * Installs the RTOS interrupt handlers.
@@ -44,8 +70,14 @@ int main(void)
     LCD_Start();
     KB_Start();
     
+    /* Create mutexes */
+    labMutex = xSemaphoreCreateMutex();
+    clueSync = xSemaphoreCreateBinary();
+
     // Create tasks
     xTaskCreate( gameTask, GAME_TASK_NAME, TASK_STACK_SIZE, NULL, GAME_PRIORITY, &gameHandler );
+    xTaskCreate( cluesTask, CLUES_TASK_NAME, TASK_STACK_SIZE, NULL, CLUES_PRIORITY, &cluesHandler );
+    xTaskCreate( medicineTask, MEDICINE_TASK_NAME, TASK_STACK_SIZE, NULL, MEDICINE_PRIORITY, &medicineHandler );
     
     // Launch freeRTOS
     vTaskStartScheduler();     
@@ -82,7 +114,41 @@ void releaseContamination( void ){
  * 
  */
 void releaseClue( Token clue ){
-    /* TODO Complete this */
+    currentClue = clue;
+    xSemaphoreGive(clueSync);
 }
+
+
+/*
+ * Used to schedule vaccine production.
+ */
+void cluesTask( void * args){
+
+    for(;;){
+        xSemaphoreTake(clueSync, portMAX_DELAY); // Wait for a new clue
+        
+        //Check if there are enough pills to start vaccine production
+        if(getMedicineCntr() < PILLS_REQUIRED) continue;
+
+        // Assign the lab to the vaccine research
+        xSemaphoreTake(labSemaphore, portMAX_DELAY);
+        shipVaccine( assignMissionToLab(currentClue) );
+        xSemaphoreGive(labSemaphore);
+    }
+
+}
+
+/*
+ * Used to schedule medicine production.
+ */
+void medicineTask( void * args){
+    for(;;){
+        // Assign the lab to the medicine production
+        xSemaphoreTake(labSemaphore, portMAX_DELAY);
+        shipMedicine( assignMissionToLab(0) );
+        xSemaphoreGive(labSemaphore);
+    }
+}
+
 
 /* [] END OF FILE */
